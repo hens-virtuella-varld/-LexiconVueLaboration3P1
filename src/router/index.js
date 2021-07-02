6613077471f8110de8e847993414b0dc3908062c
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2021-07-02 16:24:18 +0200

    add the route 'createPost'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import CreatePost from '../views/CreatePost.vue'
+
+
 
 Vue.use(VueRouter)
 
@@ -17,6 +20,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/createPost',
+    name: 'CreatePost',
+    component: CreatePost
   }
 ]
 
