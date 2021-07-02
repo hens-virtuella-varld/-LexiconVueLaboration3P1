2258e842d89d0569a080616e96025f4a53f07677
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2021-07-02 17:00:16 +0200

    import showBlogs component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,18 @@
 <template>
-    <div class="home"></div>
+    <div class="home">
+        <show-blogs></show-blogs>
+    </div>
 </template>
+
+<script>
+import showBlogs from "@/components/showBlogs";
+export default {
+    components: {
+        "show-blogs": showBlogs,
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
