a1e4e78f5f64021c114312bd4e95b4dbcd0245e9
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2021-07-02 16:59:41 +0200

    import Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <Navbar></Navbar>
+        <router-view />
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+    components: {
+        Navbar,
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
